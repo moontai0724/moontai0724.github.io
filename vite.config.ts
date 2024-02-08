47fd459b02f6d6237b76f3ee19a172cf59ad2e19
moontai0724 <moontai0724@gmail.com>
2024-02-08 14:13:07 +0800

    style: setup rules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import typescript from "@rollup/plugin-typescript";
+import vue from "@vitejs/plugin-vue";
+import { readdirSync } from "fs";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const root = "src";
+
+const alias = readdirSync(resolve(__dirname, root)).reduce((acc, value) => {
+  acc[value] = resolve(__dirname, `${root}/${value}`);
+
+  return acc;
+}, {});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    typescript({
+      tsconfig: `./tsconfig.json`,
+    }),
+  ],
+  resolve: {
+    alias,
+  },
+});
